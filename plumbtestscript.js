5e7f8ffddb9d6b3b12b6d3dd9dffcd45aa431684
dzou <dzou@terpmail.umd.edu>
2015-09-27 11:35:01 -0400

    adding plumbjs stuff

--- plumbtestscript.js
+++ plumbtestscript.js
@@ -1,11 +1,122 @@
 var elementWidth = 100;
 var elementHeight = 100;
 
+var componentType = "function";
+var arrowType = "dataArrow";
+
+var graph = {};
+
+function addNode(nodeId, name) {
+  entry = {};
+  entry["data"] = [];
+  entry["event"] = [];
+  entry["name"] = name;
+  if (componentType == "function") {
+    entry["type"] = "function";
+  } else {
+    entry["type"] = "component";
+    entry["componentType"] = componentType;
+  }
+
+  graph[nodeId] = entry;
+  console.log(graph);
+}
+
+function addEdge(nodeStart, nodeEnd) {
+  var list;
+  if (arrowType == "dataArrow") {
+    list = graph[nodeStart]["data"];
+  } else {
+    list = graph[nodeStart]["event"];
+  }
+
+  list.push(nodeEnd);
+}
+
+function removeNode(nodeId) {
+  for (var id in graph) {
+    entry = graph[id];
+    for (var i = entry["data"].length; i >= 0; i--) {
+      if (entry["data"][i] == nodeId) {
+        entry["data"].splice(i, 1);
+      }
+    }
+
+    for (var i = entry["event"].length; i >= 0; i--) {
+      if (entry["event"][i] == nodeId) {
+        entry["event"].splice(i, 1);
+      }
+    }
+  }
+  delete graph[nodeId];
+
+  console.log(graph); 
+}
+
+function clearAllNodes() {
+  graph = {};
+  $('#container').empty();
+}
+
+function resetPlumbDefaults() {
+  var arrowColor = "rgba(128, 0, 128, 0.5)";
+  if (arrowType == "dataArrow") {
+    arrowColor = "rgba(128, 0, 128, 0.5)";
+  } else if (arrowType = "eventArrow") {
+    arrowColor = "rgba(0, 102, 0, 0.5)";
+  }
+
+  jsPlumb.ready(function() {
+    jsPlumb.importDefaults({
+      PaintStyle : {
+        lineWidth:7,
+        strokeStyle: arrowColor
+      } 
+    });
+  });
+}
+
+// Set up onclick from buttons to change variables
+$(document).ready(function() {
+  $('#functionComponent').click(function() {
+    componentType = 'function';
+    resetPlumbDefaults();
+    console.log(componentType);
+  });
+
+  $('#textComponent').click(function() {
+    componentType = 'text';
+    resetPlumbDefaults();
+    console.log(componentType);
+  });
+
+  $('#buttonComponent').click(function() {
+    componentType = 'button';
+    resetPlumbDefaults();
+    console.log(componentType);
+  });
+
+  $('#dataArrow').click(function() {
+    arrowType = 'dataArrow';
+    resetPlumbDefaults();
+    console.log(arrowType);
+  });
+
+  $('#eventArrow').click(function() {
+    arrowType = 'eventArrow';
+    resetPlumbDefaults();
+    console.log(arrowType);
+  });
+
+  $('#resetButton').click(function() {
+    clearAllNodes();
+  });
+});
+
 jsPlumb.ready(function() {
   jsPlumb.setContainer($('#container'));
-
   var i = 0;
-
+  resetPlumbDefaults();
   jsPlumb.Defaults.Overlays = [
     [ "Arrow", { 
         location:1,
@@ -15,11 +126,28 @@ jsPlumb.ready(function() {
     } ]
   ];
 
+  jsPlumb.bind("connection", function(info, originalEvent) {
+    console.log(info.sourceId);
+    var sourceId = $("#" + info.sourceId).parent().attr('id');
+    var targetId = $("#" + info.targetId).parent().attr('id');
+    console.log(sourceId);
+    addEdge(sourceId, targetId);   
+  });
+
+  // Figure out how to add connectors
+
   $('#container').dblclick(function(e) {
-      var newState = $('<div>').attr('id', 'state' + i).addClass('item');
+      var nodeId = componentType + i;
+      var name = $("#componentNameBox").val() 
+      if (name == null || name.length == 0) {
+        name = nodeId;
+      }
+
+      var newState = $('<div>').attr('id', nodeId).addClass('item');
     
-      var title = $('<div>').addClass('title').text('State ' + i);
-      var connect = $('<div>').addClass('connect');
+      var title = $('<div>').addClass('title').text(name);
+      var connectId = 'connectstate' + i;
+      var connect = $('<div>').attr('id', connectId).addClass('connect');
       
       newState.css({
         'top': e.pageY,
@@ -31,7 +159,7 @@ jsPlumb.ready(function() {
       
       $('#container').append(newState);
       
-      jsPlumb.makeTarget(newState, {
+      jsPlumb.makeTarget(connect, {
         anchor: 'Continuous',
         connector: 'StateMachine'
       });
@@ -47,12 +175,14 @@ jsPlumb.ready(function() {
       });
 
       newState.dblclick(function(e) {
-        jsPlumb.detachAllConnections($(this));
+        jsPlumb.detachAllConnections($(connect));
         $(this).remove();
         e.stopPropagation();
+        removeNode(nodeId);
       });   
       
+      addNode(nodeId, name);
       i++;    
+      console.log(jsPlumb.getConnections());
   });
-
 });
